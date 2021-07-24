e20ee59523eb6bbe182dc21365e20496e1e6c19f
Marcos Murilo <marcosmeschial@gmail.com>
2021-07-24 14:03:21 -0300

    Corrigindo as atividades JS

--- ex2.js
+++ ex2.js
@@ -1,10 +1,21 @@
-function media(...distancia) {
- var total = 0;
- for (var i = 0; i<distancia.length; i++) {
-    total+=distancia[i];
- }
-console.log('A média do Jerônimo é ' + total/distancia.length);
+function media(qtd) {
+   var soma = 0;
+   var count = 0;
+
+   while (count < qtd) {
+      soma += getRandomInt(1, 10);
+      count++;
+   }
+
+   var total = soma/qtd;
+   console.log('A média do Jerônimo é ' + total);
 }
 
- media(10,8); 
+function getRandomInt(min, max) {
+   min = Math.ceil(min);
+   max = Math.floor(max);
+   return Math.floor(Math.random() * (max - min)) + min;
+ }
+ 
+ media(10, 8); 
 
